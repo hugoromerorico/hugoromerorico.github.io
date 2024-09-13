ef30222fab7ab79b41d86a61c9334195d70974ed
hugo-romero-mm <114697825+hugo-romero-mm@users.noreply.github.com>
2024-09-13 18:20:29 +0200

    ls to debug

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    basePath: '/hugoromerorico.github.io',
-    assetPrefix: '/hugoromerorico.github.io/',
-  };
-  
-  export default nextConfig;
-  
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/hugoromerorico.github.io',
+  assetPrefix: '/hugoromerorico.github.io/',
+};
+
+export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+
+// const isProd = process.env.NODE_ENV === 'production';
+
+// const nextConfig = {
+//   output: 'export',
+//   images: {
+//     unoptimized: true,
+//   },
+//   basePath: isProd ? '/hugoromerorico.github.io' : '',
+//   assetPrefix: isProd ? '/hugoromerorico.github.io/' : '',
+// };
+
+// export default nextConfig;

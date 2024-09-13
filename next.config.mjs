d400868d497b07540466c18ebc6535c69e3e410c
hugo-romero-mm <114697825+hugo-romero-mm@users.noreply.github.com>
2024-09-13 18:32:35 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,15 @@
 
 const isProd = process.env.NODE_ENV === 'production';
 
+console.log('Is production:', isProd); // Print whether it's in production mode
+
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true, // Needed for static exports
-  },
-  basePath: isProd ? '/hugoromerorico.github.io' : '', // Apply basePath only in production
-  assetPrefix: isProd ? '/hugoromerorico.github.io/' : '', // Apply assetPrefix only in production
+    output: 'export',
+    images: {
+        unoptimized: true, // Needed for static exports
+    },
+    basePath: isProd ? '/hugoromerorico.github.io' : '', // Apply basePath only in production
+    assetPrefix: isProd ? '/hugoromerorico.github.io/' : '', // Apply assetPrefix only in production
 };
 
 export default nextConfig;
